b0961999fdc2c1fa4e660b834a2899d90e1d8cfd
Janez Skrlj <janez321@gmail.com>
2019-03-19 23:37:53 +0100

    database connection successful

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # crawler
+##1. run docker containter
+docker run -d --name ieps-database -e POSTGRES_USER=userDB -e POSTGRES_PASSWORD=postgres -e POSTGRES_DB=postgresDB -p 5432:5432 postgres:11.2
+##2. install odbc driver for windows
+https://www.devart.com/odbc/postgresql/docs/windows.htm
--- crawler.py
+++ crawler.py
@@ -1,18 +1,20 @@
 
 import os
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.chrome.options import Options
+import pypyodbc
+
+# from selenium import webdriver
+# from selenium.webdriver.common.keys import Keys
+# from selenium.webdriver.chrome.options import Options
 
 chrome_path = 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe'
 
-def download_and_render_link(url):
-    options = Options()
-    options.add_argument("--headless")
-    options.binary_location = chrome_path
-
-    driver = webdriver.Chrome(executable_path=os.path.abspath('chromedriver'), options=options)
-    driver.get("http://fri.uni-lj.si")
+# def download_and_render_link(url):
+#     options = Options()
+#     options.add_argument("--headless")
+#     options.binary_location = chrome_path
+#
+#     driver = webdriver.Chrome(executable_path=os.path.abspath('chromedriver'), options=options)
+#     driver.get("http://fri.uni-lj.si")
 
 
 def extract_data(data):
@@ -39,6 +41,23 @@ def initialize_crawler(settings):
     return 'not implemented'
 
 
+def connect():
+    # Specifying the ODBC driver, server name, database, etc. directly
+    driver = 'Devart ODBC Driver for PostgreSQL'
+    cnxn = pypyodbc.connect('DRIVER={' + driver + '};SERVER=127.0.0.1;PORT=5432;DATABASE=postgresDB;uid=userDB;pwd=postgres;')
+    # cnxn = pypyodbc.connect('DSN=postgres;SERVER=127.0.0.1;PORT=5432;DATABASE=postgresDB;uid=userDB;pwd=postgres;')
+
+    # Create a cursor from the connection
+    return cnxn.cursor()
+
+
+
 
 if __name__ == '__main__':
-    download_and_render_link('ds')
+    connect()
+
+
+
+
+
+
