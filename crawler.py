0f455042343dc543bbe38b7f2efe9d6386ccdd30
Janez Skrlj <janez321@gmail.com>
2019-03-20 00:11:15 +0100

    crawldb.sql script run with python

--- crawler.py
+++ crawler.py
@@ -41,21 +41,39 @@ def initialize_crawler(settings):
     return 'not implemented'
 
 
+
+
 def connect():
     # Specifying the ODBC driver, server name, database, etc. directly
     driver = 'Devart ODBC Driver for PostgreSQL'
     cnxn = pypyodbc.connect('DRIVER={' + driver + '};SERVER=127.0.0.1;PORT=5432;DATABASE=postgresDB;uid=userDB;pwd=postgres;')
     # cnxn = pypyodbc.connect('DSN=postgres;SERVER=127.0.0.1;PORT=5432;DATABASE=postgresDB;uid=userDB;pwd=postgres;')
-
     # Create a cursor from the connection
     return cnxn.cursor()
+
+
+def read_create_db_sql(file_name,cursor):
+    with open(file_name, "r") as file:
+        sqlScript = file.read()
+        for statement in sqlScript.split(';'):
+            #ugly af but it works
+            try:
+                cursor.execute(statement+';')
+                cursor.commit()
+                print("SUCCESS: " + statement+';')
+            except:
+                print("FAILED: " + statement+';')
+
+
+
 
 
 
 
 if __name__ == '__main__':
-    connect()
-
+    cursor = connect()
+    read_create_db_sql("crawldb.sql",cursor)
+    cursor.close()
 
 
 
